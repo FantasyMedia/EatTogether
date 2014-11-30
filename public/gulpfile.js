e46abb55df898b3bc6357bfb7333275eb832df74
Fantasy Shao <forever.fantasy27@gmail.com>
2014-11-30 11:53:57 +0800

    Update gulpfile for public folder.

---
+++ public/gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * gulpfile
+ *
+ * @description
+ * @author Fantasy <fantasyshao@icloud.com>
+ * @create 2014-11-28
+ * @update 2014-11-30
+ */
+
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+var paths = {
+  manage: 'sass/manage.sass'
+};
+
+gulp.task('default', ['build']);
+
+gulp.task('build', function () {
+  gulp.src(paths.manage)
+    .pipe(sass({ noCache: true, 'sourcemap=none': true }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.manage, ['build']);
+});
