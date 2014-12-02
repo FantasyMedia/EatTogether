c2c476ab1ab3f9cf4d01b717169a721944021f69
Fantasy Shao <forever.fantasy27@gmail.com>
2014-12-03 00:20:03 +0800

    Make some tries.

--- controllers/manage/index.js
+++ controllers/manage/index.js
@@ -8,8 +8,39 @@
  * @update 2014-11-29
  */
 
+var async = require('async');
+
+var Shop = require('../../service').Shop;
+var Food = require('../../service').Food;
+
 exports.index = function (req, res, next) {
 
+  var shopList = [];
+  var foodList = [];
+
+  // Get shop and food list
+  //async.parallel([
+  //
+  //], function () {
+  //
+  //});
+
   res.render('manage/index', { title: '管理后台' });
+
+};
+
+exports.addShop = function (req, res, next) {
+
+  console.log(req.query);
+
+  var name = req.query.name;
+  var address = req.query.address;
+  var tel = req.query.tel;
+
+  Shop.newAndSave(name, address, tel, function () {
+    res.send({
+      'success': true
+    });
+  });
 
 };
--- public/js/manage.js
+++ public/js/manage.js
@@ -19,10 +19,39 @@ $.extend(Manage.prototype, {
   },
 
   bindEvents: function () {
+    // Change active menu on click
     $('.sidebar li').on('click', function (e) {
       e.stopPropagation();
       $(this).addClass('active').siblings().removeClass('active');
     });
+
+    // Add shop
+    $('#doAddShop').on('click', function (e) {
+      e.stopPropagation();
+      // TODO: Add validation
+      $.ajax({
+        url: '/addShop',
+        dataType: 'json',
+        data: {
+          name: $('#shopName').val(),
+          tel: $('#shopTel').val(),
+          address: $('#shopAddress').val()
+        },
+        success: function (res) {
+          if (res.success === true) {
+           console.log('Add shop success');
+          }
+        }
+      });
+
+      return false;
+    });
+
+    // Add food
+    $('#doAddFood').on('click', function () {
+
+    });
+
   }
 
 });
